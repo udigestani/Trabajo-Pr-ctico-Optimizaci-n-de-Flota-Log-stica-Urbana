13686f6d205301038775aa6de7d3d1e2d140c5a7
udigestani <udigestani@itba.edu.ar>
2026-08-29 00:42:08 -0300

    fuera de alcance

--- viaje.py
+++ viaje.py
@@ -14,6 +14,7 @@ class Viaje:
         Viaje.curr_id += 1
         self.id = Viaje.curr_id
 
+    #Quizas lo registra la parada
     def registrar_incidente(self, tipo, fecha, descripcion):
         self.incidente = Incidente(tipo, fecha, descripcion)
         return self.incidente
@@ -22,16 +23,19 @@ class Viaje:
         parada = Parada(orden, hora_prev, hora_real, ubicacion)
         self.paradas.append(parada)
         return parada
-    # aca lo que pense es primero ver cuanto pesa, despues con el peso ver cual de las tres opciones puede llevar ese peso. De ahi elegimos el tipo con un setter. El tema tmb es que el peso se calcula multiplicando el atributo peso de la clase articulo y sumando todos los articulos. Entonces tenemso que hacer un getter y un quilombito mas. Todo eso para setear el tipo de vehiculo. no? que opinan? dsp la velocidad la podemos calcular. entonces no se pasa como un atributo. Ahora lo saco 
-    def setter_Transporte(self,peso, costo_viaje):
-        if peso>=1000:
-            self.transporte = "Camion"
-            factor_ambiental=1.5
-        elif peso<1000 and peso>500:
-            self.transporte ="Furgoneta"
-            factor_ambiental=1.0
-        else:
-            self.transporte ="Moto"
-            factor_ambiental=0.5
-        self.transporte = Transporte( peso, costo_viaje, factor_ambiental, tipo)
-        return self.transporte
+
+    #Fuera de alcance la asignación de flota??
+    # # aca lo que pense es primero ver cuanto pesa, despues con el peso ver cual de las tres opciones puede llevar ese peso. De ahi elegimos el tipo con un setter. El tema tmb es que el peso se calcula multiplicando el atributo peso de la clase articulo y sumando todos los articulos. Entonces tenemso que hacer un getter y un quilombito mas. Todo eso para setear el tipo de vehiculo. no? que opinan? dsp la velocidad la podemos calcular. entonces no se pasa como un atributo. Ahora lo saco 
+    # def setter_Transporte(self,peso, costo_viaje):
+    #     if peso>=1000:
+    #         self.transporte = "Camion"
+    #         factor_ambiental=1.5
+    #     elif peso<1000 and peso>500:
+    #         self.transporte ="Furgoneta"
+    #         factor_ambiental=1.0
+    #     else:
+    #         self.transporte ="Moto"
+    #         factor_ambiental=0.5
+    #     self.transporte = Transporte( peso, costo_viaje, factor_ambiental, tipo)
+    #     return self.transporte
+    
