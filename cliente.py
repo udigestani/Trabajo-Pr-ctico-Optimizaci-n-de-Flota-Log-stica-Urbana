9dc3d0dd4c033f25271ddf5a946af82180d958b4
udigestani <udigestani@itba.edu.ar>
2026-09-10 17:39:49 -0300

    Creacion de clase cliente

---
+++ cliente.py
@@ -0,0 +1,32 @@
+class Cliente:
+     
+    def __init__(self, nombre: str, dni: int, edad: int):
+        self.historial = []
+        self.nombre = nombre
+        self.dni = self.validar_numero(dni)
+        self.edad = self.validar_edad(edad)
+
+    @staticmethod
+    def validar_edad(numero):
+        if isinstance(numero, int):
+            if numero > 0:
+                return numero
+            raise ValueError(f"El número {numero} debe ser mayor a 0")
+        raise TypeError(f"El número {numero} debe ser un número")
+
+    @staticmethod
+    def validar_dni(numero):
+        if isinstance(numero, int):
+            if numero > 0:
+                return numero
+            raise ValueError(f"El número {numero} debe ser mayor a 0")
+        raise TypeError(f"El número {numero} debe ser un número")
+    #HAY QUE VALIDAR QUE NO REPITA
+
+    @staticmethod
+    def validar_nombre(nombre):
+        if isinstance(nombre, str):
+            if nombre:
+                return nombre
+            raise ValueError(f"El nombre no puede ser vacío")
+        raise TypeError(f"El nombre {nombre} debe ser una cadena str")
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-# Tu implementacion va aqui
+from comprobante import Comprobante
+from articulo import Articulo
+from ubicacion import Ubicacion 
+from cliente import Cliente
+from incidente import Incidente
+from parada import Parada
+from solicitud import Solicitud
+from transporte import Transporte
+from viaje import Viaje
+
 def hola_mundo():
     return "hola_mundo"     
         
@@ -10,3 +19,10 @@ def main():
 # No cambiar a partir de aqui
 if __name__ == "__main__":
     main()
+
+
+    #LISTAS ENLAZADAS NO VA
+    #DICCIONARIO SE PUEDE, LISTA DE LISTAS NO SE PUEDE
+    #TESTING SI --> 3 CLASES TESTEADAS, UNA CON MUCHO CONTENIDO (IMPORTANTE)
+    #USAR CONCEPTOS COMO MOCK, ASSERT, Y QUE NO SEA REDUNDANTE
+    #TIENE QUE ANDAR, ESTAR COMPLETO Y HACER UNA PARTE BÁSICA DEL ENUNCIADO
